ffb9b491dc79b095e72f74638cfd3aebf6ccd13f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-04 13:03:12 -0300

    fix: update to test description name

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,7 +4,7 @@ import { BcryptAdapter } from './bcrypt-adapter'
 describe('Bcrypt Adapter', () => {
   // teste para garantir a integração do Bcrypt Adapter
   // com a biblioteca
-  test('Should call bcrypt with correct value', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
