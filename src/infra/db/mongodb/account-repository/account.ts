a87c3137cda67b21641169e86eb12880f49ef6a0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-05 01:42:00 -0300

    feat: ensure AccountMongoRepository an account on sucess

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,15 @@
+import type { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import type { AccountModel } from '../../../../domain/models/account'
+import type { AddAccountModel } from '../../../../domain/usecase/add-account'
+import { MongoHelper } from '../helper/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = result.ops[0]
+    const { _id, ...other } = account
+    console.log(account)
+    return Object.assign({}, other, { id: _id })
+  }
+}
