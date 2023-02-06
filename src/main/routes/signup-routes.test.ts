46c2cd3d6ce27a19b0ca76de0bce0f35fb5d1bd0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-06 00:41:00 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import { app } from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 
 describe('Sigmup Routes', () => {
+  // conectar com o banco durante os testes
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  // desconectar do banco depois dos testes
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  // entre um teste e outro esse metodo limpa as tabelas
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
